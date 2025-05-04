0c77f55699b5e22d0206bda08774b38e1ab14384
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-05-04 16:45:38 -0300

    fix bug in while

--- index.js
+++ index.js
@@ -1,37 +1,60 @@
-import { select } from "@inquirer/prompts";
+import { select, input } from "@inquirer/prompts";
+
+let meta = {};
+
+let metas = [meta];
+
+const cadastrarMeta = async () => {
+    const meta = await input({ message: "Digite a meta" });
+
+    if (meta.length == 0) {
+        console.log("A meta não pode ser vazia");
+        return;
+    }
+
+    metas.push({
+        value: meta,
+        checked: false,
+    });
+};
+
+const listarMetas = async () => {};
 
 const start = async () => {
-    const option = await select({
-        message: "Menu",
-        choices: [
-            {
-                name: "Cadastrar meta",
-                value: "cadastrar",
-            },
-            {
-                name: "Listar metas",
-                value: "listar",
-            },
-            {
-                name: "Sair",
-                value: "sair",
-            },
-        ],
-    });
-
     while (true) {
+        const option = await select({
+            message: "Menu",
+            choices: [
+                {
+                    name: "Cadastrar meta",
+                    value: "cadastrar",
+                },
+                {
+                    name: "Listar metas",
+                    value: "listar",
+                },
+                {
+                    name: "Sair",
+                    value: "sair",
+                },
+            ],
+        });
+
         switch (option) {
             case "cadastrar":
+                await cadastrarMeta();
+                console.log(metas);
                 break;
 
             case "listar":
+                listarMetas();
                 break;
 
             case "sair":
                 return;
-                break;
 
             default:
+                console.log("Até a proxima!");
                 break;
         }
     }
