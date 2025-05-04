7edd44539f86b7e4b98fc59af7b2b083e102973e
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-05-03 21:16:16 -0300

    init

---
+++ index.js
@@ -0,0 +1,16 @@
+// Criador de Metas //
+
+let meta = {
+    value: "Ler um livro por mês",
+    checked: true,
+};
+
+let metas = [
+    meta,
+    {
+        value: "Fazer exercícios",
+        checked: false,
+    },
+];
+
+console.log(metas[1].value);
