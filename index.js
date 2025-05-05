1aa5353bc6a65672338e058f7d12a4bd874cea8d
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-05-05 03:49:27 -0300

    fix some bugs in the methods

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import { select, input, checkbox } from "@inquirer/prompts";
 
-let meta = {};
-
-let metas = [meta];
+let metas = [];
 
 const cadastrarMeta = async () => {
     const meta = await input({ message: "Digite a meta" });
@@ -19,6 +17,12 @@ const cadastrarMeta = async () => {
 };
 
 const listarMetas = async () => {
+    if (metas.length == 0) {
+        console.log("Nenhuma meta foi cadastrada!");
+
+        return;
+    }
+
     const respostas = await checkbox({
         message:
             "Use as setas para mudar de meta, o espaço para marcar ou desmarcar e o enter para finalizar essa etapa",
@@ -57,8 +61,25 @@ const metasRealizadas = async () => {
     }
 
     await select({
-        message: "Metas Realizadas",
+        message: `Metas Realizadas ${realizadas.length}`,
         choices: [...realizadas],
+    });
+};
+
+const metasAbertas = async () => {
+    const abertas = metas.filter((meta) => {
+        return meta.checked != true;
+    });
+
+    if (abertas.length == 0) {
+        console.log("Não existem metas abertas!");
+
+        return;
+    }
+
+    await select({
+        message: `Metas Abertas ${abertas.length}`,
+        choices: [...abertas],
     });
 };
 
@@ -80,6 +101,10 @@ const start = async () => {
                     value: "realizadas",
                 },
                 {
+                    name: "Metas abertas",
+                    value: "abertas",
+                },
+                {
                     name: "Sair",
                     value: "sair",
                 },
@@ -97,6 +122,10 @@ const start = async () => {
 
             case "realizadas":
                 await metasRealizadas();
+                break;
+
+            case "abertas":
+                await metasAbertas();
                 break;
 
             case "sair":
