8ec95d9a74eaeef0b488ab243c30a484761ce02e
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-05-06 04:03:11 -0300

    add file system for save and load goals

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 import { select, input, checkbox } from "@inquirer/prompts";
 
-let metas = [];
+import fs from "node:fs/promises";
+
+let metas;
 
 let mensagem = "Bem Vindo ao App de Metas!";
+
+const carregarMetas = async () => {
+    try {
+        const dados = await fs.readFile("metas.json", "utf-8");
+        metas = JSON.parse(dados);
+    } catch (error) {
+        metas = [];
+    }
+};
+
+const salvarMetas = async () => {
+    await fs.writeFile("metas.json", JSON.stringify(metas, null, 2));
+};
 
 const cadastrarMeta = async () => {
     const meta = await input({ message: "Digite a meta" });
@@ -123,8 +138,11 @@ const mostrarMensagem = () => {
 };
 
 const start = async () => {
+    await carregarMetas();
+
     while (true) {
         mostrarMensagem();
+        await salvarMetas();
 
         const option = await select({
             message: "Menu",
